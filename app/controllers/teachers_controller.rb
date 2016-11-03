382cde92d01c913d2b736c4b598169d55ab85c97
Antti Tolppanen <antti.tolppanen@aalto.fi>
2016-11-03 14:24:53 +0200

    Created a controller for handling searching teachers based on skills and levels

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -2,4 +2,12 @@ class TeachersController < ApplicationController
 	def show
 		@teacher_id = params[:id]
 	end
+
+	def index
+		#Find relevan teachers, the query should include subject and level presented as integers correspnding
+		#to specific values in the Skill-model
+		@allteachers = User.where(teacher: true)
+		@teachers = @allteachers.includes(:skills).where(skills: { subject: 'params[:subject]', level: 'params[:level]'  })
+	end
+
 end
