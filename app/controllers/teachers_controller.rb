c297316f545c64278c6ec49e63a203dee4d24599
lauraniemi <laura.niemi@aalto.fi>
2016-11-07 17:04:22 +0200

    Tein teachers-viewiin index-sivun hakutuloksille.

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -7,7 +7,7 @@ class TeachersController < ApplicationController
 		#Find relevan teachers, the query should include subject and level presented as integers correspnding
 		#to specific values in the Skill-model
 		@allteachers = User.where(teacher: true)
-		@teachers = @allteachers.includes(:skills).where(skills: { subject: 'params[:subject]', level: 'params[:level]'  })
+		#@teachers = @allteachers.includes(:skills).where(skills: { subject: 'params[:subject]', level: 'params[:level]'  })
 	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,5 @@ Rails.application.routes.draw do
 	root to: "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'teachers/:id' => 'teachers#show'
+  get 'teachers' => 'teachers#index'
 end
