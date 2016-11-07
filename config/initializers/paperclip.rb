bfc3b7ff1801631ba5111053b3949d625d3a138b
Antti Tolppanen <antti.tolppanen@aalto.fi>
2016-11-07 15:25:05 +0200

    AWS regions change

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,3 +1,4 @@
-# # config/initializers/paperclip.rb
-# Paperclip::Attachment.default_options[:url] = ':tutorinki.s3.amazonaws.com'
-# Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style'
+# config/initializers/paperclip.rb
+Paperclip::Attachment.default_options[:url] = ':tutorinki.s3.amazonaws.com'
+Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
+Paperclip::Attachment.default_options[:s3_host_name] = 'eu-west-1.amazonaws.com'
