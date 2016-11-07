189170cd06bdede553d33672d18249c8b18739df
Antti Tolppanen <antti.tolppanen@aalto.fi>
2016-11-07 15:28:39 +0200

    AWS stuff again

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,4 +1,4 @@
 # config/initializers/paperclip.rb
 Paperclip::Attachment.default_options[:url] = ':tutorinki.s3.amazonaws.com'
 Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
-Paperclip::Attachment.default_options[:s3_host_name] = 'eu-west-1.amazonaws.com'
+Paperclip::Attachment.default_options[:s3_host_name] = 's3-eu-west-1.amazonaws.com'
