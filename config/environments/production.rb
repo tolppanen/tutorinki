cabd77ad0ae7b516cc8fc4c8ce72580ecefce72d
Antti Tolppanen <antti.tolppanen@aalto.fi>
2016-12-22 16:50:42 +0200

    Modified setting to make chat work on Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,8 +48,9 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+   config.action_cable.url = "wss://tutorinki.herokuapp.com/cable"
+   config.action_cable.allowed_request_origins = ['https://tutorinki.herokuapp.com',
+                                               'http://tutorinki.herokuapp.com']
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
