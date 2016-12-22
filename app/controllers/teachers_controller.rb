1458b528ef35c92be5c468660e49e71b29a3cc7f
Antti Tolppanen <antti.tolppanen@aalto.fi>
2016-12-22 19:09:14 +0200

    More advanced search features

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -20,40 +20,55 @@ class TeachersController < ApplicationController
 		#to specific values in the Skill-model
 		@query = params[:subject_query]
 		@detail = params[:detail_query]
+		@resultarray = []
+		@subjects = []
 		@results = User.where(teacher: true).all
 		if @query != nil
 			if @query != ""
 				@query.downcase!
-				if @detail != nil
-					@subject = Subject.where("name LIKE :search", search: "%#{@query}%").where(detail: @detail).first
+				if @detail != ""
+					@subject = Subject.where("name LIKE :search", search: "%#{@query}%").where("detail LIKE :search", search: "%#{@detail}%").first
+					@subject.users.where(teacher: true).all.each do |u|
+						@resultarray << u
+					end
 				else
-						@subject = Subject.where("name LIKE :search", search: "%#{@query}%").first
+					@subjects = Subject.where("name LIKE :search", search: "%#{@query}%").all
+					@subjects.each do |s|
+						s.users.where(teacher: true).all.each do |u|
+							if !@resultarray.include?(u)
+							@resultarray << u
+						 end
+						end
+					end
+					puts @resultarray.size
 				end
 				if @subject == nil
 					@results = User.where(teacher: true).all
-				else
-					@results = @subject.users.where(teacher: true).all
+				end
+					#@results = @subject.users.where(teacher: true).all
+					puts @resultarray
+					@results = @resultarray
 				end
 			end
-		 end
 		end
 
 
-		def bio
-			if current_user.teacher?
-				@teacher = current_user
-				@skills = @teacher.skills
-				@newskill = Skill.new
-			else
-				redirect_to root_path
-			end
-		end
 
-		def save
-			puts "==============="
-			puts params
-			current_user.update!(bio: params[:user][:bio], city: params[:user][:city])
-			redirect_to :controller => 'teachers', :action => 'bio'
+	def bio
+		if current_user.teacher?
+			@teacher = current_user
+			@skills = @teacher.skills
+			@newskill = Skill.new
+		else
+			redirect_to root_path
 		end
+	end
+
+	def save
+		puts "==============="
+		puts params
+		current_user.update!(bio: params[:user][:bio], city: params[:user][:city])
+		redirect_to :controller => 'teachers', :action => 'bio'
+	end
 
 end
